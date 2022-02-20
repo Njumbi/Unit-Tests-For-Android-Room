710ce452b6e478979a7f1f2eb84d0dba7294385b
Njumbi <wambuinjumbi@gmail.com>
2022-02-20 17:25:07 +0300

    style: design second screen

---
+++ app/src/main/java/com/example/words/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.words.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.words.R
+import com.example.words.databinding.ActivityAddNameBinding
+import com.example.words.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fab.show()
+
+        binding.fab.setOnClickListener {
+            val intent = Intent(this, AddNameActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
