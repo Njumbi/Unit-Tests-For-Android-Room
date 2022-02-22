0a6a4fd7e868d5b28ade2ad9e892abc4c2cf524f
Njumbi <wambuinjumbi@gmail.com>
2022-02-22 21:16:21 +0300

    feature: create app file

---
+++ app/src/main/java/com/example/words/App.kt
@@ -0,0 +1,10 @@
+package com.example.words
+
+import android.app.Application
+import com.example.words.data.AppDatabase
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App: Application() {
+
+}
