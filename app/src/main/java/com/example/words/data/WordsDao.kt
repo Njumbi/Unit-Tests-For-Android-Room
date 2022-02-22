4c77c1370a4530196688f3b058da412efbea847f
Njumbi <wambuinjumbi@gmail.com>
2022-02-22 21:18:11 +0300

    feat:set up android room

--- app/src/main/java/com/example/words/data/WordsDao.kt
+++ app/src/main/java/com/example/words/data/WordsDao.kt
@@ -1,8 +1,8 @@
 package com.example.words.data
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
@@ -13,13 +13,17 @@ interface WordsDao {
     @Query("SELECT * FROM `word`")
     fun getAllWords(): Flow<List<Word>>
 
-    @Query("DELETE  FROM `word`")
+    @Query("SELECT * FROM `word` where id = :wordId")
+    suspend fun getOneWord(wordId: Int): Word
+
+    @Query("DELETE FROM word")
     suspend fun nuke()
 
     @Insert
-   suspend fun insertWord(word: Word)
+    suspend fun insertWord(word: Word)
 
-    @Update(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun updateWord( word: Word)
+    @Update
+    suspend fun updateWord(word: Word)
+
 
 }
---
+++ app/src/main/java/com/example/words/repository/wordsRepository.kt
@@ -0,0 +1,30 @@
+package com.example.words.repository
+
+import com.example.words.data.Word
+import com.example.words.data.WordsDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class wordsRepository @Inject constructor(val wordsDao: WordsDao){
+
+    suspend fun insertWord(word: Word){
+        wordsDao.insertWord(word)
+    }
+
+    suspend fun deleteWord(){
+        wordsDao.nuke()
+    }
+
+
+    fun fetchAllWords() : Flow<List<Word>> {
+        return wordsDao.getAllWords()
+            .flowOn(Dispatchers.Default)
+            .conflate()
+    }
+
+
+
+}
